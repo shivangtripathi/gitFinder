33fa1b5573f911fad0df4163cf055077c229ed6e
shivangtripathi <shivangtripathi2000@gmail.com>
2020-08-02 22:35:50 +0530

    Updates

---
+++ precache-manifest.0269189475f1bd54d758c914ed00d481.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a435c156a30f3b0330495bd05ffdd6ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11c8863c4f59b401b068",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0f6f9da8fe001d3e02e3",
+    "url": "/static/css/main.58bfab35.chunk.css"
+  },
+  {
+    "revision": "11c8863c4f59b401b068",
+    "url": "/static/js/2.0b863b24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b863b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f6f9da8fe001d3e02e3",
+    "url": "/static/js/main.fddc0a39.chunk.js"
+  },
+  {
+    "revision": "25d48e391914dc08b45e",
+    "url": "/static/js/runtime-main.d89612ff.js"
+  },
+  {
+    "revision": "92e88315d6b8b4118ce515e734d55d56",
+    "url": "/static/media/Component 1.92e88315.png"
+  }
+]);
